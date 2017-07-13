a854c6e44a0cb150cbc5a358e064435214ded171
tomoyukikashiro <ksrtmyk@gmail.com>
2017-07-12 20:42:37 -0400

    use css

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path  =  require('path');
 
 module.exports = {
+  context: path.resolve(__dirname, './'),
   entry: './scripts/app.js',
   output: {
     path: path.resolve(__dirname, 'dist/scripts'),
     filename: 'bundle.js'
   },
-  devtool: 'cheap-module-source-map',
   resolve: {
     alias: {
       jquery: '../node_modules/jquery/src/jquery.js'
     }
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-source-map'
 }
